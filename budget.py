4cac2015998106c7e4d7753771eec0dac6507fc5
hernan2587 <76715969+hernan2587@users.noreply.github.com>
2026-01-28 18:04:42 -0600

    Add files via upload

---
+++ budget.py
@@ -0,0 +1,30 @@
+"""
+
+Personal Budget Calculator
+Calculate expenses and percentaje of income spent.
+
+"""
+
+# 1. Get Input
+gross_pay = float(input("Enter monthly Gross pay: $"))
+
+rent = float(input("Enter monthly rent: $"))
+insunrance = float(input("Enter monthly insurance: $"))
+gas = float(input("Enter monthly gas: $"))
+phone = float(input("Enter monthly phone: $"))
+gym =float(input("Enter monthly gym: $"))
+
+# 2. Total Exoenses, remaining, and income
+
+total_expenses = rent + insunrance + gas + phone + gym
+remaining_balance = gross_pay - total_expenses
+percent_spent = (total_expenses / gross_pay) * 100
+
+print("Monthly Budget Summary")
+print("Total Expenses: $", format(total_expenses, ".2f"))
+print("Remaining Balance: $", format(remaining_balance, ".2f"))
+print("Percentage Spent: ", format(percent_spent, ".2f"), "%")
+
+
+
+
